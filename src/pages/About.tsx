90b0af8b532411386d3ba7b638e0d9588e5b3bc9
Vaibhav <vaibhavbaheti28@gmail.com>
2024-01-04 23:02:00 +0530

    about page changes and browser

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,12 +3,18 @@ import Wrapper from "../sections/Wrapper";
 import avatarImage from "../assets/me.jpg";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import LaunchRoundedIcon from "@mui/icons-material/LaunchRounded";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 function About() {
   return (
     <div className="profile">
       <img src={avatarImage} alt="ME" className="profile-image" />
       <h1 className="profile-text">Hi I am Vaibhav Baheti</h1>
-      <h2 className="profile-text">I optimized this awesome pokedex</h2>
+      <h2 className="profile-text">
+        I optimized this awesome pokedex. Older Site:
+        <a href="https://vaibhavbaheti28.github.io/react-pokedex/pokemon/1">
+          <ArrowOutwardIcon />
+        </a>
+      </h2>
 
       <div className="profile-links">
         <a href="https://github.com/VaibhavBaheti28?tab=repositories">
